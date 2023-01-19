f31e82c9ac92f32d265ded2a91be43c8e310dd10
PedroQDV <pedro.dozzi10@gmail.com>
2023-01-19 04:17:43 -0300

    Primeiro Commit - Projeto com React

---
+++ src/Styles.js
@@ -0,0 +1,21 @@
+//Importar a biblioteca "styled-componets", para usarmos o ccs com javascript.
+import styled from 'styled-components'
+
+export const Container = styled.div `
+`
+export const Image = styled.img `
+`
+
+export const ContainerItens = styled.div `
+`
+
+export const H1 = styled.h1 `
+`
+
+export const InputLabel = styled.p `
+`
+
+export const Input = styled.Input `
+`
+export const Button = styled.button `
+`
