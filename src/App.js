bb5f0ea261eae913483ae0751658304c62fcab90
PedroQDV <pedro.dozzi10@gmail.com>
2023-01-20 16:55:00 -0300

    Criando a pagina de Cadastro.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React from "react";
+
+import Figure1 from './assets/Figure1.svg'
+import Seta from './assets/Seta.svg'
+
 import { Container, Image, ContainerItens, H1, InputLabel, Input, Button } from './Styles';
 
 
 //Isso é JSX(A mistura de html com javascript).
 const App = () => {
-  
+
+  const users = [{ id: Math.random(), name: "Pedro", age: 19 },
+  { id: Math.random(), name: "Ana", age: 18 }
+  ]
+
   return (
 
-    <Container> 
+    <Container>
 
-      <Image />
+      <Image alt="logo-image" src={Figure1} />
 
       <ContainerItens>
         <H1> Olá! </H1>
 
         <InputLabel>Nome</InputLabel>
-        <Input placeholder="Nome"/>
+        <Input placeholder="Nome" />
 
         <InputLabel>Idade</InputLabel>
-        <Input placeholder="Idade"/>
+        <Input placeholder="Idade" />
 
-        <Button>Cadastrar</Button>
+        <Button>Cadastrar <img alt="seta" src={Seta} /></Button>
+
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.age}
+            </li>
+          ))
+          }
+        </ul>
 
       </ContainerItens>
-      
-
     </Container>
   );
 }
